2561c63a85b4cc4b47e29339c97a6ad6776f1851
msgerasyov <msgerasyov@edu.hse.ru>
2022-01-07 22:41:12 +0300

    Update UNet

--- tdm/models/unet/unet.py
+++ tdm/models/unet/unet.py
@@ -2,9 +2,33 @@ import torch
 import torch.nn as nn
 
 
-class UNet(nn.Module):
-    def __init__(self):
-        print("Created UNet")
+class ConvBlock(nn.Sequential):
+    def __init__(self, in_channels, out_channels):
+        layers = [
+            nn.Conv2d(in_channels, out_channels, 3),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, 3),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, 3),
+            nn.ReLU()
+        ]
+        super().__init__(*layers)
 
-    def forward(self, X):
+
+class UpConv(nn.Sequential):
+    def __init__(self, in_channels):
+        out_channels = in_channels // 2
+        layers = [
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(in_channels, out_channels, 2)
+        ]
+        super().__init__(*layers)
+
+
+class UNet(nn.Module):
+    def __init__(self, in_channels, n_classes, hid_size=64):
+        print("Created UNet")
+        super().__init__()
+
+    def forward(self, x):
         pass
