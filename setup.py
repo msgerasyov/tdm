499a392f30ed74e9e245e9a5d2f2f2adee8f4fa2
msgerasyov <72410668+msgerasyov@users.noreply.github.com>
2022-01-11 22:01:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='tdm',
     version='0.1',
-    description='PyTorch Deep Models',
+    description='Unofficial PyTorch implementation of some key deep models',
     author='Matvey Gerasyov',
     packages=find_packages(exclude=['tests']),
     install_requires=['torch >= 1.4', 'torchvision'],
